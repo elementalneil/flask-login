05a488ea08be693e95476f78366d83983fe89fa9
elementalneil <nayek.sankhanil@gmail.com>
2022-10-24 19:51:41 +0530

    Created Database Operations for Login and Signup

---
+++ login_operations.py
@@ -0,0 +1,93 @@
+import bcrypt
+import sqlite3
+
+# The following class handles all database operations related to logging in and signing up
+class LoginPage:
+    # The constructor establishes connection to the database if it exists
+    # Otherwise it creates the database
+    def __init__(self):
+        self.db_connection = sqlite3.connect("LoginProject.sqlite")  # Establishes connection to database file
+        self.db_cursor = self.db_connection.cursor()    # Cursor to connection object. Allows us to interact with db.
+
+        # SQL Queries to set up database if it does not exist
+        init_script = '''
+            CREATE TABLE IF NOT EXISTS Accounts (
+                username VARCHAR2(50) NOT NULL UNIQUE PRIMARY KEY,
+                password VARCHAR2(50) NOT NULL
+            )
+        '''
+
+        # The executescript function takes a script, executes it, returns a new cursor object
+        self.db_cursor.executescript(init_script)       # Executes create table script
+
+    # Creates a new account from given username and password
+    # If username is already taken, returns false
+    def create_account(self, username, password):
+        # The execute function takes a single SQL statement, executes it, returns a new cursor object
+        res = self.db_cursor.execute('SELECT * FROM Accounts WHERE username = ?', (username, ))
+        # cursor.fetchone() returns one tuple from the relation that the cursor contains
+        row = res.fetchone()
+
+        # Create account will only occur if username exists
+        if(row == None):
+            # The next two lines encrypt the given password
+            password = bytes(password, 'utf-8')
+            hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+            # The username and encrypted password are stored into the database.
+            self.db_cursor.execute('INSERT INTO Accounts VALUES(?, ?)', (username, hashed_password))
+            self.db_connection.commit()     # Remember to always commit the changes
+
+            return True
+        else:
+            return False
+
+    def login(self, username, password):
+        res = self.db_cursor.execute('SELECT * FROM Accounts WHERE username = ?', (username, ))
+        row = res.fetchone()
+        # row is a list where each element represents each attribute of the tuple
+
+        status = 0   # status is 1 if login successful, 2 if username does not exist, 3 if incorrect password.
+        # Login will only occur if the username exists
+        if(row != None):
+            # Now, we will check if the given password matches the password in the database
+            password = bytes(password, 'utf-8')
+            if(bcrypt.checkpw(password, row[1])):
+                status = 1
+            else:
+                status = 3
+        else:
+            status = 2
+        
+        return status
+
+
+def main():
+    login_obj = LoginPage()
+
+    choice = input('Enter 1 for signup, 2 for login: ')
+    
+    if choice == '1':
+        username = input('Username: ')
+        password = input('Password: ')
+
+        if login_obj.create_account(username, password):
+            print('Account Created')
+        else:
+            print('Username is taken')
+    
+    else:
+        username = input('Username: ')
+        password = input('Password: ')
+
+        status = login_obj.login(username, password)
+
+        if status == 1:
+            print('Logged In')
+        elif status == 2:
+            print('Account does not exist')
+        else:
+            print('Incorrect Password')
+
+if __name__ == "__main__":
+    main()
